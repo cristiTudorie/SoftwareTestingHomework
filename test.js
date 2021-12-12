cbb65a87291727fafed4ef15360f072b0ae9fb44
cristiTudorie <73597663+cristiTudorie@users.noreply.github.com>
2021-12-12 17:15:49 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,18 @@
+
+function sum(x, y) {
+    return x + y;
+}
+
+
+function test(x, y) {
+    console.log(`The sum function returns ${sum(x, y)}`)
+    console.log(`The expected result should be ${x + y}`)
+    if(sum(x, y) !== x + y) {
+        console.log(`The sum function is wrong.`)
+    }
+    else {
+        console.log(`The sum function is right.`)
+    }
+}
+
+test(10, 20)
